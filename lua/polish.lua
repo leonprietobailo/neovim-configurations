84857095b9c90e4d41eeffd74ad3e4609a57500b
Prieto Bailo, León Enrique <contact@leonprieto.com>
2026-05-23 20:38:31 +0200

    Disable arrow keys and mouse

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,20 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.opt.mouse = ""
+
+local disabled_keys = {
+  "<Up>",
+  "<Down>",
+  "<Left>",
+  "<Right>",
+}
+
+for _, key in ipairs(disabled_keys) do
+  vim.keymap.set({ "n", "i", "x", "s", "o", "c", "t" }, key, "<Nop>", {
+    desc = "Disabled arrow key",
+    noremap = true,
+    silent = true,
+  })
+end
